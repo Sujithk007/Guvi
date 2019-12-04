ac46078d97479911802b79702306a0600a152091
Sujithk007 <49073723+Sujithk007@users.noreply.github.com>
2019-12-04 18:54:12 +0530

    Update 546.py

--- 546.py
+++ 546.py
@@ -1,18 +1,4 @@
-''' 
-You are given a large number made of only 0’s and 1’s.Your task is to find the max no of consecutive 1’s. If there are no 1’s print -1
 
-Input Description:
-A large number ‘n’
-
-Output Description:
-Print the max no of consecutive 1 in the number
-
-Sample Input :
-101011111
-
-Sample Output :
-5
-'''
 r=list(input())
 max1,m=0,0
 for i in range(len(r)):
