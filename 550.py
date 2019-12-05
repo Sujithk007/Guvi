9b1d53685948f257f0c96a1fb290116aa3d0a138
Sujithk007 <49073723+Sujithk007@users.noreply.github.com>
2019-12-05 11:31:51 +0530

    Create 550.py

---
+++ 550.py
@@ -0,0 +1,16 @@
+'''
+You are given a series(In example). Go through the series in order to find the answer.You are given with a number ‘n’. Find the nth number of series.
+
+Input Description:
+You are given a number ‘n’
+
+Output Description:
+Print the nth number of series.
+
+Sample Input :
+6
+
+Sample Output :
+21
+'''
+print(sum([i for i in range(1,int(input())+1)]))
