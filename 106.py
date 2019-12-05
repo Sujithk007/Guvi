47a1d055cadc552aa7b6dbd681b015c7df12a654
Sujithk007 <49073723+Sujithk007@users.noreply.github.com>
2019-12-05 11:44:37 +0530

    Create 106.py

---
+++ 106.py
@@ -0,0 +1,12 @@
+'''
+Given an angle A, print the sine of the given angle.
+Sample Testcase :
+INPUT
+30
+OUTPUT
+0.5
+'''
+import math 
+m=180/int(input())
+a = math.pi /m 
+print (round(math.sin(a),1)) 
